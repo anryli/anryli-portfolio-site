777ca0d910ddcc4ff5071a229513af8c54fb2697
anryli <anryhasegawalee@163.com>
2024-12-22 23:48:50 +0900

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export', // Ensures static output for GitHub Pages
   assetPrefix: './', // Ensures assets work correctly with relative paths
   trailingSlash: true, // Adds a trailing slash to paths
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
