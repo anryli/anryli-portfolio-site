106bd1121f5e5ec2c74c5a493e7ece6bdc96e515
anryli <anryhasegawalee@163.com>
2024-12-22 23:47:56 +0900

    add export options

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Ensures static output for GitHub Pages
+  assetPrefix: './', // Ensures assets work correctly with relative paths
+  trailingSlash: true, // Adds a trailing slash to paths
 };
 
 export default nextConfig;
